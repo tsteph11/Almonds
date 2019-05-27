a8429d610d26ea376deffae53ef0b0fcd9f87770
Travis Stephens <tstephens010@gmail.com>
2019-05-27 10:21:38 -0400

    initial push

---
+++ Assets/TextState.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "TextState")]
+public class TextState : ScriptableObject
+{
+    [TextArea(10, 14)]
+    [SerializeField] public string Script;
+    [SerializeField] public string[] choices;
+    [SerializeField] public TextState[] nextStates;
+
+    public string GetStateStory() {
+        return Script;
+    }
+
+    public TextState[] GetNextStates() {
+        return nextStates;
+    }
+
+    public string[] GetChoices() {
+        return choices;
+    }
+}
